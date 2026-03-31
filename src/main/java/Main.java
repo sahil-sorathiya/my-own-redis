3b72b9d455d6081fca5d13cd1bda81388de2d9f2
Sahil Patel <sahil.patel4125@gmail.com>
2026-03-31 16:18:02 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,28 +8,19 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            // Handle client in new thread
-            new ClientHandler(clientSocket).start();
+            while(true){
+                Socket clientSocket = null;
+                clientSocket = serverSocket.accept();
+                new ClientHandler(clientSocket).start();
+            }
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
         }
     }
 }
