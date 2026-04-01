8ee42e3b6c247cdfdaaebc2debf575ad1f274856
Sahil Patel <sahil.patel4125@gmail.com>
2026-04-01 16:49:10 +0530

    codecrafters submit [skip ci]

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class ClientHandler extends Thread {
     private final Socket clientSocket;
@@ -17,15 +18,26 @@ public class ClientHandler extends Thread {
 
     ConcurrentHashMap<String, ArrayList<String>> hm1;
     ConcurrentHashMap <String, ArrayList<String>> hm2;
+    ConcurrentHashMap<String, String> hm3;
     LinkedHashSet <Long> lhs1;
+    ConcurrentHashMap<String, ConcurrentSkipListMap<String, ConcurrentHashMap<String, String>>> hm4;
 
-    ClientHandler(Socket clientSocket, ConcurrentHashMap<String, ArrayList<String>> hm1, ConcurrentHashMap<String, ArrayList<String>> hm2, LinkedHashSet<Long> lhs1) throws IOException {
+    ClientHandler(
+            Socket clientSocket,
+            ConcurrentHashMap<String, ArrayList<String>> hm1,
+            ConcurrentHashMap<String, ArrayList<String>> hm2,
+            ConcurrentHashMap<String, String> hm3,
+            LinkedHashSet<Long> lhs1,
+            ConcurrentHashMap<String, ConcurrentSkipListMap<String, ConcurrentHashMap<String, String>>> hm4
+    ) throws IOException {
         this.clientSocket = clientSocket;
         this.inputStream = clientSocket.getInputStream();
         this.outputStream = clientSocket.getOutputStream();
         this.hm1 = hm1;
         this.hm2 = hm2;
+        this.hm3 = hm3;
         this.lhs1 = lhs1;
+        this.hm4 = hm4;
     }
 
     private String getInput() throws IOException {
@@ -84,8 +96,9 @@ public class ClientHandler extends Thread {
         String s = command.get(0);
         if(s.equalsIgnoreCase("echo")){
             outputStream.write(("$" + command.get(1).length() + sep + command.get(1) + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("set")){
+        if(s.equalsIgnoreCase("set")){
             String key = command.get(1);
             String val = command.get(2);
             hm1.put(command.get(1), new ArrayList<>());
@@ -95,189 +108,237 @@ public class ClientHandler extends Thread {
                 String timestamp = Instant.now().plusMillis(Integer.parseInt(ms)).toString();
                 hm1.get(key).add(timestamp);
             }
+            hm3.put(key, "string");
             outputStream.write(("+OK" + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("get")){
+        if(s.equalsIgnoreCase("get")){
             String key = command.get(1);
-            if(hm1.containsKey(key)){
-                ArrayList<String> val = hm1.get(key);
-                if(val.size() > 1){
-                    Instant t1 = Instant.now();
-                    Instant t2 = Instant.parse(val.get(1));
-                    if(t1.isAfter(t2)){
-                        hm1.remove(key);
-                        outputStream.write(("$-1" + sep).getBytes());
-                    }
-                    else{
-                        outputStream.write(("$" + val.get(0).length() + sep + val.get(0) + sep).getBytes());
-                    }
-                }
-                else{
-                    outputStream.write(("$" + val.get(0).length() + sep + val.get(0) + sep).getBytes());
-                }
-            }
-            else {
+            if(!hm1.containsKey(key)){
                 outputStream.write(("$-1" + sep).getBytes());
+                return;
             }
+            ArrayList<String> val = hm1.get(key);
 
-            Thread.currentThread().threadId();
+            if(val.size() > 1){
+                Instant t1 = Instant.now();
+                Instant t2 = Instant.parse(val.get(1));
+                if(t1.isAfter(t2)){
+                    hm1.remove(key);
+                    hm3.remove(key);
+                    outputStream.write(("$-1" + sep).getBytes());
+                    return;
+                }
+            }
+            outputStream.write(("$" + val.get(0).length() + sep + val.get(0) + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("rpush")){
+        if(s.equalsIgnoreCase("rpush")){
             String key = command.get(1);
             List<String> vals = command.subList(2, command.size());
 
             if(!hm2.containsKey(key)){
                 hm2.put(key, new ArrayList<>());
+                hm3.put(key, "list");
             }
             for(String val: vals){
                 hm2.get(key).add(val);
             }
             outputStream.write((":" + hm2.get(key).size() + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("lrange")){
+        if(s.equalsIgnoreCase("lrange")){
             String key = command.get(1);
             if(!hm2.containsKey(key)){
                 outputStream.write(("*0" + sep).getBytes());
+                return;
             }
-            else{
-                ArrayList<String> vals = hm2.get(key);
-                int l = Integer.parseInt(command.get(2));
-                int r = Integer.parseInt(command.get(3));
+            ArrayList<String> vals = hm2.get(key);
+            int l = Integer.parseInt(command.get(2));
+            int r = Integer.parseInt(command.get(3));
 
-                if(l < 0) {
-                    if(l < -1*vals.size()) l = 0;
-                    else l = vals.size() + l;
-                }
-
-                if(r < 0){
-                    if(r < -1*vals.size()) r = 0;
-                    else r = vals.size() + r;
-                }
-
-                if(l >= vals.size() || l > r){
-                    outputStream.write(("*0" + sep).getBytes());
-                }
-                else{
-                    ArrayList <String> temp = new ArrayList<>();
-                    for(int i = l; i <= r && i < vals.size(); i++){
-                        temp.add("$" + vals.get(i).length() + sep + vals.get(i) + sep);
-                    }
-                    StringBuilder res = new StringBuilder("*" + temp.size() + sep);
-                    for(String t: temp){
-                        res.append(t);
-                    }
-                    outputStream.write(res.toString().getBytes());
-                }
+            if(l < 0) {
+                if(l < -1*vals.size()) l = 0;
+                else l = vals.size() + l;
             }
 
+            if(r < 0){
+                if(r < -1*vals.size()) r = 0;
+                else r = vals.size() + r;
+            }
+
+            if(l >= vals.size() || l > r){
+                outputStream.write(("*0" + sep).getBytes());
+                return;
+            }
+            ArrayList <String> temp = new ArrayList<>();
+            for(int i = l; i <= r && i < vals.size(); i++){
+                temp.add("$" + vals.get(i).length() + sep + vals.get(i) + sep);
+            }
+            StringBuilder res = new StringBuilder("*" + temp.size() + sep);
+            for(String t: temp){
+                res.append(t);
+            }
+            outputStream.write(res.toString().getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("lpush")){
+        if(s.equalsIgnoreCase("lpush")){
             String key = command.get(1);
             List<String> vals = command.subList(2, command.size());
 
             if(!hm2.containsKey(key)){
                 hm2.put(key, new ArrayList<>());
+                hm3.put(key, "list");
             }
             for(String val: vals){
                 hm2.get(key).addFirst(val);
             }
             outputStream.write((":" + hm2.get(key).size() + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("llen")){
+        if(s.equalsIgnoreCase("llen")){
             String key = command.get(1);
-            if(!hm2.containsKey(key)) outputStream.write((":0"+sep).getBytes());
-            else outputStream.write((":" + hm2.get(key).size() + sep).getBytes());
+            if(!hm2.containsKey(key)) {
+                outputStream.write((":0"+sep).getBytes());
+                return;
+            }
+            outputStream.write((":" + hm2.get(key).size() + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("lpop")){
+        if(s.equalsIgnoreCase("lpop")){
             String key = command.get(1);
             int count = 0;
             if(command.size() == 3) count = Integer.parseInt(command.get(2));
             if(!hm2.containsKey(key)){
                 outputStream.write(("$-1" + sep).getBytes());
+                return;
             }
-            else if(count == 0){
+            if(count == 0){
                 String val = hm2.get(key).removeFirst();
+                if(hm2.get(key).isEmpty()) {
+                    hm2.remove(key);
+                    hm3.remove(key);
+                }
                 outputStream.write(("$" + val.length() + sep + val + sep).getBytes());
+                return;
             }
-            else{
-                ArrayList <String> temp = new ArrayList<>();
-                while(count > 0 && hm2.get(key).size() > 0){
-                    count--;
-                    temp.add(hm2.get(key).removeFirst());
-                }
-                StringBuilder res = new StringBuilder("*" + temp.size() + sep);
-                for(String t: temp){
-                    res.append("$" + t.length() + sep + t + sep);
-                }
-                outputStream.write(res.toString().getBytes());
+            ArrayList <String> temp = new ArrayList<>();
+            while(count > 0 && hm2.get(key).isEmpty()){
+                count--;
+                temp.add(hm2.get(key).removeFirst());
             }
+            if(hm2.get(key).isEmpty()) {
+                hm2.remove(key);
+                hm3.remove(key);
+            }
+            StringBuilder res = new StringBuilder("*" + temp.size() + sep);
+            for(String t: temp){
+                res.append("$" + t.length() + sep + t + sep);
+            }
+            outputStream.write(res.toString().getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("blpop")){
+        if(s.equalsIgnoreCase("blpop")){
             String key = command.get(1);
             double seconds = Double.parseDouble(command.get(2));
 
             if(!hm2.containsKey(key)){
                 hm2.put(key, new ArrayList<>());
+                hm3.put(key, "list");
             }
-            ArrayList<String> temp = hm2.get(key);
-            if(temp.isEmpty()){
-                if(seconds == 0){
-                    synchronized (lhs1){
-                        lhs1.add(Thread.currentThread().threadId());
-                    }
-                    while(temp.isEmpty() || lhs1.getFirst() != Thread.currentThread().threadId());
-                    synchronized (lhs1){
-                        lhs1.removeFirst();
-                    }
-                    String val = temp.removeFirst();
-                    outputStream.write(("*2" + sep + "$" + key.length() + sep + key + sep + "$" + val.length() + sep + val + sep).getBytes());
-                }
-                else{
-                    Instant expiry = Instant.now().plusMillis((long)(seconds * 1000));
-                    synchronized (lhs1){
-                        lhs1.add(Thread.currentThread().threadId());
-                    }
 
-                    boolean isExpired = false;
-                    while(temp.isEmpty() || lhs1.getFirst() != Thread.currentThread().threadId()){
-                        Instant now = Instant.now();
-                        if(now.isAfter(expiry)) {
-                            isExpired = true;
-                            break;
-                        }
-                    }
-                    if(isExpired){
-                        synchronized (lhs1){
-                            lhs1.remove(Thread.currentThread().threadId());
-                        }
-                        outputStream.write(("*-1" + sep).getBytes());
-                    }
-                    else{
-                        synchronized (lhs1){
-                            lhs1.removeFirst();
-                        }
-                        String val = temp.removeFirst();
-                        outputStream.write(("*2" + sep + "$" + key.length() + sep + key + sep + "$" + val.length() + sep + val + sep).getBytes());
-                    }
-                }
-            }
-            else {
+            ArrayList<String> temp = hm2.get(key);
+            if(!temp.isEmpty()){
                 String val = temp.removeFirst();
                 outputStream.write(("*2" + sep + "$" + key.length() + sep + key + sep + "$" + val.length() + sep + val + sep).getBytes());
+                return;
             }
+
+            if(seconds == 0){
+                synchronized (lhs1){
+                    lhs1.add(Thread.currentThread().threadId());
+                }
+                while(temp.isEmpty() || lhs1.getFirst() != Thread.currentThread().threadId());
+                synchronized (lhs1){
+                    lhs1.removeFirst();
+                }
+                String val = temp.removeFirst();
+                outputStream.write(("*2" + sep + "$" + key.length() + sep + key + sep + "$" + val.length() + sep + val + sep).getBytes());
+                return;
+            }
+
+            Instant expiry = Instant.now().plusMillis((long) (seconds * 1000));
+            synchronized (lhs1) {
+                lhs1.add(Thread.currentThread().threadId());
+            }
+            boolean isExpired = false;
+            while (temp.isEmpty() || lhs1.getFirst() != Thread.currentThread().threadId()) {
+                Instant now = Instant.now();
+                if (now.isAfter(expiry)) {
+                    isExpired = true;
+                    break;
+                }
+            }
+
+            if (isExpired) {
+                synchronized (lhs1) {
+                    lhs1.remove(Thread.currentThread().threadId());
+                }
+                outputStream.write(("*-1" + sep).getBytes());
+                return;
+            }
+
+            synchronized (lhs1) {
+                lhs1.removeFirst();
+            }
+            String val = temp.removeFirst();
+            outputStream.write(("*2" + sep + "$" + key.length() + sep + key + sep + "$" + val.length() + sep + val + sep).getBytes());
+            return;
         }
-        else if(s.equalsIgnoreCase("type")){
+        if(s.equalsIgnoreCase("type")){
             String key = command.get(1);
-            if(!hm1.containsKey(key)){
+            if(!hm3.containsKey(key)){
                 outputStream.write(("+none" + sep).getBytes());
             }
             else {
-                outputStream.write(("+string" + sep).getBytes());
+                outputStream.write(("+" + hm3.get(key) + sep).getBytes());
             }
         }
-        else {
-            outputStream.write(("+PONG" + sep).getBytes());
+        if(s.equalsIgnoreCase("xadd")){
+            String streamName = command.get(1);
+            String streamId = command.get(2);
+
+            if(!hm4.containsKey(streamName)){
+                hm4.put(streamName, new ConcurrentSkipListMap<>());
+            }
+
+            ConcurrentSkipListMap<String, ConcurrentHashMap<String, String>> currentSLM = hm4.get(streamName);
+            String lastEnteredStreamId = (currentSLM.isEmpty()) ? "0-0" : currentSLM.lastEntry().getKey();
+
+            // if streamId == 0-0 throw error
+            if(streamId.equals("0-0")){
+                outputStream.write(("-ERR The ID specified in XADD must be greater than 0-0" + sep).getBytes());
+                return;
+            }
+
+            // if lastEnteredStreamId >= streamId throw error
+            if(lastEnteredStreamId.compareTo(streamId) >= 0){
+                outputStream.write(("-ERR The ID specified in XADD is equal or smaller than the target stream top item" + sep).getBytes());
+                return;
+            }
+
+            // store the key-vals
+            currentSLM.put(streamId, new ConcurrentHashMap<>());
+            for(int i = 3; i < command.size(); i += 2){
+                currentSLM.get(streamId).put(command.get(i), command.get(i+1));
+            }
+
+            // store type of stream
+            hm3.put(streamName, "stream");
+            outputStream.write(("$" + streamId.length() + sep + streamId + sep).getBytes());
+
         }
+        outputStream.write(("+PONG" + sep).getBytes());
         return;
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,19 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("Logs from your program will appear here!");
 
-        ConcurrentHashMap<String, ArrayList<String>> hm1 = new ConcurrentHashMap<>();
-        ConcurrentHashMap<String, ArrayList<String>> hm2 = new ConcurrentHashMap<>();
-        LinkedHashSet<Long> lhs1 = new LinkedHashSet<Long>();
+        ConcurrentHashMap<String, ArrayList<String>> hm1 = new ConcurrentHashMap<>(); // For <key, <val, timestamp>> pairs
+        ConcurrentHashMap<String, ArrayList<String>> hm2 = new ConcurrentHashMap<>(); // For <key, list>
+        ConcurrentHashMap<String, String> hm3 = new ConcurrentHashMap<>(); // For <key, type> pairs
+        LinkedHashSet<Long> lhs1 = new LinkedHashSet<Long>(); // Queue of threads blocked in "BLPOP" command
+        ConcurrentHashMap<String, ConcurrentSkipListMap<String, ConcurrentHashMap<String, String>>> hm4 = new ConcurrentHashMap<>(); // For streams <streamName, <streamId, <key, val>>>
         ServerSocket serverSocket = null;
 
         int port = 6379;
@@ -22,7 +24,7 @@ public class Main {
             while(true){
                 Socket clientSocket = null;
                 clientSocket = serverSocket.accept();
-                new ClientHandler(clientSocket, hm1, hm2, lhs1).start();
+                new ClientHandler(clientSocket, hm1, hm2, hm3, lhs1, hm4).start();
             }
 
         } catch (IOException e) {
